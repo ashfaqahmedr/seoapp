9dd17b20a88e4d3d86502b44831fa0998614ae8a
ASHFAQ AHMED RAJPUT <135718829+ashfaqahmedr@users.noreply.github.com>
2023-08-05 16:18:38 +0500

    Update apifunctions.js

--- apifunctions.js
+++ apifunctions.js
@@ -168,22 +168,11 @@ async function fetchArticleCreatorbyStatus(articleStatus) {
   // Show Animation
   showLoader();
 
+  try {
   // Task 1: Fetch all projects and store the required keys in an object
-  fetch(`${seourl}/all-projects?status=${articleStatus}`)
-    .then((response) => response.json())
-    .then(async (data) => {
-      const formattedData = {};
-
-      // Extract the required keys from each project
-      data.result.forEach((project) => {
-        const { id, name, type, status } = project;
-        formattedData[id] = { 
-          projectID: id,
-          projectName: name,
-          projectType: type,
-          projectStatus: status
-        };
-      });
+  const response = await fetch(`${seourl}?action=getStatus&sheetName=Projects&status=${articleStatus}`)
+   console.log(`${seourl}?action=getStatus&sheetName=Projects&status=${articleStatus}`)
+    const formattedData = await response.json();
 
       // Task 6: Generate the table based on the final filteredProjects object
       if (Object.keys(formattedData).length > 0) {
@@ -199,8 +188,8 @@ async function fetchArticleCreatorbyStatus(articleStatus) {
         let text = `No projects found for the specified article status "${articleStatus}".`;
         createToast(type, icon, title, text);
       }
-    })
-    .catch((error) => {
+    }
+    catch(error) {
       console.error(error);
       // Call createToast function after error
       let type = 'error';
@@ -208,83 +197,20 @@ async function fetchArticleCreatorbyStatus(articleStatus) {
       let title = 'Error';
       let text = 'An error occurred while fetching projects.';
       createToast(type, icon, title, text);
-    })
-    .finally(() => {
-      hideLoader();
-    });
-}
+    }
+}  
 
-
-const statusCounts = {
-  allprojects: 0,
-  creator: 0,
-  poster: 0,
-  jsoncount: 0,
-  draft: 0,
-  waiting: 0,
-  failed: 0,
-  aborted: 0,
-  complete: 0,
-  running: 0,
-  aborting: 0,
-};
-
-const statusUrls = {
-  allprojects: `${seourl}/all-projects`,
-  creator: `${seourl}/all-projects?type=article%20creator`,
-  poster: `${seourl}/all-projects?type=post%20uploader`,
-  draft: `${seourl}/all-projects?status=draft`,
-  waiting: `${seourl}/all-projects?status=waiting`,
-  failed: `${seourl}/all-projects?status=failed`,
-  aborted: `${seourl}/all-projects?status=aborted`,
-  complete: `${seourl}/all-projects?status=complete`,
-  running: `${seourl}/all-projects?status=running`,
-  aborting: `${seourl}/all-projects?status=aborting...`,
-};
-
-
-function updateStatusCounts() {
-
-  const promises = [];
-
-  for (const status in statusUrls) {
-    const url = statusUrls[status];
-    const promise = fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        statusCounts[status] = data.result.length;
-        updateDashboardCounts();
-      })
-      .catch((error) => {
-        console.error(`Error fetching ${status} projects:`, error);
-      });
-
-    promises.push(promise);
+async function updateStatusCounts() {
+  try {
+    const response = await fetch(`${seourl}?action=getProjectCounts&sheetName=Projects`);
+    const data = await response.json();
+    updateDashboardCounts(data); // Assuming `statusCounts` is available in the scope where this function is called.
+  } catch (error) {
+    console.error('Error fetching ID count:', error);
   }
-
-  // Separate API call to count IDs
-  const idCountPromise = fetch(`${appurl}/data`)
-    .then((response) => response.json())
-    .then((data) => {
-      statusCounts.jsoncount = data.length;
-      updateDashboardCounts();
-    })
-    .catch((error) => {
-      console.error('Error fetching ID count:', error);
-    });
-
-  promises.push(idCountPromise);
-
-  Promise.all(promises)
-    .then(() => {
-      console.log('All API calls completed.');
-    })
-    .catch((error) => {
-      console.error('Error in API calls:', error);
-    });
 }
 
-function updateDashboardCounts() {
+function updateDashboardCounts(counts) {
   const statusSpans = {
     allprojects: document.getElementById('allproject-count'),
     creator: document.getElementById('creator-count'),
@@ -299,12 +225,17 @@ function updateDashboardCounts() {
     aborting: document.getElementById('aborting-count'),
   };
 
-  for (const status in statusCounts) {
+  for (const status in counts) {
     if (statusSpans.hasOwnProperty(status)) {
-      statusSpans[status].textContent = statusCounts[status];
+      statusSpans[status].textContent = counts[status];
     }
   }
 }
+
+
+  
+
+
 
 
 
